f27a7529342da70292ea4c57e66f9fd4481df0ae
rohit6611 <84310554+rohit6611@users.noreply.github.com>
2025-07-15 12:29:52 +0530

    Update AlgoClass.java

--- Java-Interview/AlgoClass.java
+++ Java-Interview/AlgoClass.java
@@ -44,7 +44,7 @@ public class AlgoClass {
 	
 	public static void insertionSort(int arr[],int n) {
 		
-		for(int i=1;i<n-1;i++) {
+		for(int i=1;i<n;i++) {
 			int key=arr[i];
 			int j=i-1;
 			while(j>=0 && arr[j]>key) {
